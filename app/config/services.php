ac95fcf3c85b58fb883454d49e6adefe01b3d39a
Mateus Demboski <mateus.demboski@digitaldesk.com.br>
2017-04-10 15:32:22 -0300

    Remove DB service

--- app/config/services.php
+++ app/config/services.php
@@ -57,30 +57,6 @@ $di->setShared('view', function () {
     return $view;
 });
 
-/**
- * Database connection is created based in the parameters defined in the configuration file
- */
-$di->setShared('db', function () {
-    $config = $this->getConfig();
-
-    $class = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
-    $params = [
-        'host'     => $config->database->host,
-        'username' => $config->database->username,
-        'password' => $config->database->password,
-        'dbname'   => $config->database->dbname,
-        'charset'  => $config->database->charset
-    ];
-
-    if ($config->database->adapter == 'Postgresql') {
-        unset($params['charset']);
-    }
-
-    $connection = new $class($params);
-
-    return $connection;
-});
-
 
 /**
  * If the configuration specify the use of metadata adapter use it or use memory otherwise
